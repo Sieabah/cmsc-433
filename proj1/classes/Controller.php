cff30ceee70efccb3c105503198a03a8b7fdc90d
sieabah <christopher.sidell@gmail.com>
2016-04-13 01:26:34 -0400

    Remove extra semicolon

--- proj1/classes/Controller.php
+++ proj1/classes/Controller.php
@@ -6,7 +6,7 @@ class Controller extends BaseClass
     public function index(){
         $sesh = Session::get('taken', []);
 
-        $data['allClasses'] = app()->studentclass->getList($sesh);;
+        $data['allClasses'] = app()->studentclass->getList($sesh);
         $data['available'] = app()->studentclass->availableClasses($sesh);
 
         return view()->make('view', $data);
