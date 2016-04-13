4d5a4312aa0039c8b74a14b4c1a698b3319b7a3e
sieabah <christopher.sidell@gmail.com>
2016-04-13 00:23:46 -0400

    Fix typo

--- proj1/database/migrations/01_create_tables.php
+++ proj1/database/migrations/01_create_tables.php
@@ -5,7 +5,7 @@ class create_tables
     public function run()
     {
 		//Create the class list.
-        app()->raw("CREATE TABLE `classes` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, "
+        app()->db->raw("CREATE TABLE `classes` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, "
                         ."`name` CHAR(255) NULL DEFAULT NULL, "
                         ."`course` CHAR(50) NULL DEFAULT NULL, "
                         ."`number` SMALLINT NULL DEFAULT NULL, "
