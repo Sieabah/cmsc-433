3e9bcbf94c8774c00832c8d6d48b33b8eb6ec1c2
sieabah <christopher.sidell@gmail.com>
2016-04-14 00:38:01 -0400

    Add more session functionality

--- proj1/classes/Session.php
+++ proj1/classes/Session.php
@@ -8,14 +8,28 @@ class Session extends BaseClass
         session_destroy();
     }
 
-    public static function put($key, $val){
+    public static function put($key, $val = null){
         $_SESSION[$key] = $val;
     }
 
-    public static function get($key, $default){
+    public static function push($key, $val = null){
+        $arr = self::get($key);
+        $arr[] = $val;
+        self::put($key, $arr);
+    }
+
+    public static function get($key, $default = null){
         if(isset($_SESSION[$key]))
             return $_SESSION[$key];
         else
             return $default;
+    }
+
+    public static function forget($key){
+        if(is_array($key))
+            foreach($key as $name)
+                unset($_SESSION[$name]);
+        else
+            unset($_SESSION[$key]);
     }
 }
