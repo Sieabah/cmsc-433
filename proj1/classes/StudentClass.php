83de353dcdbe26f8af65443b6abe707a5ef86f31
sieabah <christopher.sidell@gmail.com>
2016-04-14 01:09:09 -0400

    Correctly check against taken list and update to associative

--- proj1/classes/StudentClass.php
+++ proj1/classes/StudentClass.php
@@ -11,12 +11,15 @@ class StudentClass extends DB
     public function getList(){
         $classes = $this->all();
 
-        foreach($classes as $class) {
+        foreach($classes as $key => $class) {
             $class->prereq = [];
             $prereq = $this->prerequisite($class->id);
             foreach ($prereq as $req) {
                 $class->prereq[] = $req;
             }
+
+            $classes[strtolower($class->course)] = $class;
+            unset($classes[$key]);
         }
 
         return $classes;
@@ -25,14 +28,16 @@ class StudentClass extends DB
     public function availableClasses($taken){
         $classes = $this->getList();
         $canTake = [];
+        $takenList = array_flip($taken);
+
         foreach($classes as $class){
             $pass = 0;
             foreach($class->prereq as $req){
-                if(isset($taken[$req->course]))
+                if(isset($takenList[$req->course]))
                     $pass++;
             }
 
-            if(count($class->prereq) == $pass)
+            if(count($class->prereq) == $pass && !isset($takenList[$class->course]))
                 $canTake[] = $class;
         }
 
