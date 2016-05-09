81f44351dfc6eb32cf4b60c775a56cc8505912ee
sieabah <christopher.sidell@gmail.com>
2016-05-09 08:39:36 -0400

    Move logic out of view into app controller

--- proj2/classes/App.php
+++ proj2/classes/App.php
@@ -18,14 +18,54 @@ class App{
     }
 
     public static function process(){
+        $data = [];
+        $input = new css_Input($_POST);
 
+        $errs = Validator::validatePost($input);
+        $data['errs'] = $errs = !empty($errs) ? $errs : null;
+
+        /**
+         * Inserts records into session after sanitizing them
+         * @author Joshua Standiford
+         */
+        if(is_null($errs)){
+            $fields = ['name', 'email', 'phone', 'campusid'];
+            
+            foreach($fields as $field){
+                session()->put('field', $input->input($field, null));
+            }
+
+            /**
+             * This function grabs pertinent information from database, creates
+             * an associative array with course name as key, and credits as val
+             * Returns array, containing associative array with credit info based
+             * on classes taken.
+             * @author Joshua Standiford
+             */
+            $courses = $input->input('course', []);
+            $creditArr = [];
+            $catalog = app()->studentclass->getList();
+
+            $taken = [];
+            foreach($courses as $class){
+                if(isset($catalog[$class])) {
+                    $taken[] = $catalog[$class]->course;
+                    $creditArr[] = $catalog[$class];
+                }
+            }
+
+            $data['canTake'] = app()->studentclass->availableClasses($taken);
+            $data['summary'] = $creditArr;
+        }
+        
+        return $data;
     }
 
     public static function route($uri){
         switch($uri){
             case '/':
                 return self::index();
-            case 'formProcess':
+            case '/formProcess':
                 return self::process();
             default:
                 return [];
@@ -37,7 +77,7 @@ class App{
             return view()->make(app()->routes[$_SERVER['REQUEST_URI']], self::route($_SERVER['REQUEST_URI']));
         }
         else
-            return view()->make($_SERVER['REQUEST_URI']);
+            return view()->make($_SERVER['REQUEST_URI'], self::route($_SERVER['REQUEST_URI']));
     }
 }
 
--- proj2/resources/views/formProcess.php
+++ proj2/resources/views/formProcess.php
@@ -1,110 +1,89 @@
-<?php
-   /**
-   * Inserts records into session after sanitizing them
-   *
-   * @author Joshua Standiford
-   */
-  function insertRecords(){
-    $ses = new Session();
-    if(($errs = Validator::validatePost(new css_Input($_POST))) != null){
-      $ses->put("name", $_POST["name"]);
-      $ses->put("email", $_POST["email"]);
-      $ses->put("phone", $_POST["contactnum"]);
-      $ses->put("campusid", $_POST["campusid"]); 
-    }
-
-  }
-
-
- function getCourseCredits(){
-    $db = new DB();
-    
-    $courseData = $db->query("SELECT course, credits FROM classes")->fetchAll(PDO::FETCH_OBJ);
-
-    foreach($courseData as $val){
-      $cArr["$val->course"] = $val->credits;
-    }
-
-    return $cArr;
- }
-
-  /**
-   * This function grabs pertinent information from database, creates
-   * an associative array with course name as key, and credits as val
-   * Returns array, containing associative array with credit info based
-   * on classes taken.
-   * @author Joshua Standiford
-   */
-  function getSummary(){
-    if(isset($_POST["course"])){
-      $courses = $_POST["course"];
-    }
-    else{
-      $courses = array();
-    }
-
-    $creditArr = array();
-
-    $cArr = getCourseCredits();
-
-    foreach($courses as $class){
-      $temp["credits"] = $cArr[$class];  
-      $temp["name"] = strtoupper($class);
-      array_push($creditArr, $temp);
-    }
-    return $creditArr;
-  }
-
-?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html>
   <head>
     <meta charset="utf8">
-    <link rel="stylesheet" href="/classList.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Computer Science Class List</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="/style.css">
   </head>
   <body>
-   <div class="header"> <a href="/"><img src="./img/umbclogo.png"></a></div>
-    <div id="classtree">
-      <?php if(($errs = Validator::validatePost(new css_Input($_POST))) != null):?>
-      <div class="error">
-        <p class="heading">The following errors prevented the data from being processed:</p>
-
-        <?php foreach($errs as $err): ?>
-          <p class="bolderr"> <?php echo $err; ?> </p>
-        <?php endforeach; ?>
-        <a class="fancyLink return" href="/">Use this link to return to the form</a>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <a href="/">
+            <img src="/img/umbclogo.png" />
+          </a>
+        </div>
       </div>
-      <?php else: insertRecords(); ?>  
-        <p class="heading">Your information has been submitted!</p>
-        <p class="bold">When you visit your advisor, they will look up the classes you have taken based on the campus id that you have provided</p>
-        <p class="bold">Your advisor will then discuss potential course options based on this information. Your summary is below: </p>
-        <hr>
-        <table id="summary">
-          <tr>
-            <th>Course Taken</th>
-            <th>Credit Value</th>
-          </tr>
+      <div class="row">
+        <div id="classtree" class="col-xs-12">
+          <?php if($errs):?>
+            <div class="error">
+              <p class="heading">The following errors prevented the data from being processed:</p>
 
-          <?php
-          $totalCredits = 0;
-          $summary = getSummary();
-          foreach($summary as $course):
-            $totalCredits += $course["credits"];
-            ?>
-            <tr>
-            <td><?php echo $course["name"]; ?></td>
-            <td><?php echo $course["credits"]; ?></td>
-            </tr>
+              <?php foreach($errs as $err): ?>
+                <p class="bolderr"> <?php echo $err; ?> </p>
+              <?php endforeach; ?>
+              <a class="fancyLink return" href="/">Use this link to return to the form</a>
+            </div>
+          <?php else: ?>
+            <h1>Your information has been submitted!</h1>
+            <h4>When you visit your advisor, they will look up the classes you have taken based on the campus id that you have provided</h4>
+            <h4>Your advisor will then discuss potential course options based on this information. Your summary is below: </h4>
+            <hr>
+            <div class="col-xs-12 col-sm-6">
+              <h3>Your summary</h3>
+              <table class="summary">
+                <tr>
+                  <th>Course Taken</th>
+                  <th>Credit Value</th>
+                </tr>
 
-          <?php endforeach; ?>
-          <tr>
-            <td class="boldTot">Total Credits: </td>
-            <td class="boldTot"><?php printf("%1\$.2f", $totalCredits); ?></td>
-          </tr>
-        </table>
-      <?php endif; ?>
+                <?php
+                $totalCredits = 0;
+                foreach($summary as $course):
+                  $totalCredits += $course->credits;
+                  ?>
+                  <tr>
+                    <td><strong><?= strtoupper($course->course); ?></strong> <?= $course->name ?></td>
+                    <td><?= $course->credits; ?></td>
+                  </tr>
+
+                <?php endforeach; ?>
+                <tr>
+                  <td class="boldTot">Total Credits: </td>
+                  <td class="boldTot"><?php printf("%1\$.2f", $totalCredits); ?></td>
+                </tr>
+              </table>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <h3>Available courses</h3>
+              <table class="summary">
+                <tr>
+                  <th>Course</th>
+                  <th>Credit Value</th>
+                </tr>
+
+                <?php
+                foreach($canTake as $course):
+                  ?>
+                  <tr>
+                    <td><strong><?= strtoupper($course->course); ?></strong> <?= $course->name ?></td>
+                    <td><?= $course->credits; ?></td>
+                  </tr>
+
+                <?php endforeach; ?>
+              </table>
+            </div>
+          <?php endif; ?>
+        </div>
+      </div>
     </div>
   </body>
 </html>
